a459f01e7df7f6a1122643c5b787554030efa084
YFLooi <yihfoo@gmail.com>
2022-01-02 15:32:18 +0800

    bootstrap global import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // Moralis settings
+  // Follows: https://docs.moralis.io/moralis-server/tools/react-moralis
+  const serverUrl = "https://gppanowoee5f.usemoralis.com:2053/server";
+  const appId = "RRTVGl64FMS7Zh4t73WEwBUqhLR9o9vDZLb3Q05Y";
+
+  return (
+    <>
+      <Head>
+        {/* Top level import for entire app */}
+        <link
+          key="bootstrap"
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+      <MoralisProvider appId={appId} serverUrl={serverUrl}>
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
