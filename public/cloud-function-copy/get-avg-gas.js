eb09b4f450d058fc4cd35151217befef54f0e049
YFLooi <yihfoo@gmail.com>
2022-01-02 15:32:46 +0800

    local copy of getAvgGas cloud function

---
+++ public/cloud-function-copy/get-avg-gas.js
@@ -0,0 +1,22 @@
+// needs to be in Moralis server as Cloud Function in order to use
+Moralis.Cloud.define("getAvgGas", async function (request) {
+  const query = new Moralis.Query("EthTransactions");
+  // This looks a lot like MongoDB's aggregation pipeline
+  const pipeline = [
+    {
+      group: {
+        // group by "from_address"
+        objectId: "$from_address",
+        // add computed property avgGas
+        // get average and convert wei to gwei
+        avgGas: { $avg: { $divide: ["$gas_price", 1000000000] } },
+      },
+    },
+    { sort: { avgGas: -1 } }, // sort by avgGas high to low
+    { limit: 10 }, // only return top 10 results
+  ];
+
+  // the master key is required for aggregate queries
+  const results = await query.aggregate(pipeline, { useMasterKey: true });
+  return results;
+});
